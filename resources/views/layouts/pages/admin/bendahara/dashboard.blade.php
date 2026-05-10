86f1ebc853c53e95a8be32be678d9d87b0e0b383
royhan76 <beensholih@gmail.com>
2026-05-10 22:56:42 +0700

    tambah master nominal bendahara

--- resources/views/layouts/pages/admin/bendahara/dashboard.blade.php
+++ resources/views/layouts/pages/admin/bendahara/dashboard.blade.php
@@ -6,7 +6,7 @@
         <div class="page-inner py-5">
             <div class="d-flex align-items-left align-items-md-center flex-column flex-md-row">
                 <div>
-                    <h2 class="text-white pb-2 fw-bold">Data Pengguna</h2>
+                    <h2 class="text-white pb-2 fw-bold">Data Nominal Pembayaran</h2>
                     <h5 class="text-white op-7 mb-2">Manajemen Akun Sistem</h5>
                 </div>
             </div>
